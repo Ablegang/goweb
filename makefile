5db66fb69455feddcfb2603a5b29d31fb53ec0ca
Ablegang <lvpg@weipaitang.com>
2020-06-23 14:18:57 +0800

    default msg

--- makefile
+++ makefile
@@ -1,6 +1,9 @@
 master:
 # 需要指定当前系统的 sh
+# make master m=message
 	@sh shell/master.sh
 
+# make feature b=logpkg_0623 f=master
+# b 指新建的分支名 f 指基本分支，默认为 master，则从 master 切出新分支
 feature:
 	@sh shell/feature.sh
--- shell/feature.sh
+++ shell/feature.sh
@@ -1,1 +1,20 @@
-if $b
+prefix="===========>"
+
+if [ "$b" = "" ]; then
+  echo "$prefix 请指定新分支名"
+  exit
+fi
+
+if [ "$(git status | grep 'nothing to commit, working tree clean')" = "" ]; then
+  echo "$prefix 当前分支有修改暂未提交，请先处理"
+  exit
+fi
+
+echo "$prefix 即将创建 feature/$b 分支"
+
+from=$f
+
+if [ "$from" = "" ]; then
+  echo "$prefix 当前未指定基础分支，默认从 master 切出新分支"
+  from="master"
+fi
