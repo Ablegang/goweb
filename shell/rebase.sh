eb73cef87cc82ff8a6985f89e799196d6075c49d
Ablegang <lvpg@weipaitang.com>
2020-06-28 17:58:05 +0800

    rebase.sh

--- shell/rebase.sh
+++ shell/rebase.sh
@@ -19,3 +19,9 @@ git checkout $NOWB
 git rebase $b
 
 echo "$prefix rebase finish"
+
+echo "$prefix 接下来的合并步骤"
+echo "      1、将冲突文件一一处理"
+echo "      2、处理完后，运行 git add ."
+echo "      3、运行 git rebase --continue"
+echo "      4、如果想放弃 rebase，可以运行 git rebase --abort"
