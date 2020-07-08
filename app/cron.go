99db2ae8731b8650abeccddb1762d82f169213b7
Ablegang <lvpg@weipaitang.com>
2020-07-08 10:43:03 +0800

    update

--- app/cron.go
+++ app/cron.go
@@ -6,6 +6,8 @@ func startCron() {
 	// 行情监控
 	go cron.ListenQuotesNotice()
 	go cron.ListenQuotesCommonPush()
+	go cron.NearCloseNotice()
+	go cron.NearOpenNotice()
 
 	// 协程始终启动，除非 main 进程关闭
 	select {}
