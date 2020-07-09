b0df2bdff9ad15502ca29925e966c5d14524656d
Ablegang <lvpg@weipaitang.com>
2020-07-09 15:21:43 +0800

    让command支持env

--- pkg/commands/quotes.go
+++ pkg/commands/quotes.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"goweb/app/cron/quotes"
+	_ "goweb/pkg/env"
+	"os"
 )
 
 func main() {
 	// 直接使用 make quotes ，并未载入 .env ，所以直接写死
-	u := quotes.GetQuotes("0600345,0000001,0000300,0600036,1002776,0603101,0600110,1000672,0603799,1000032")
+	u := quotes.GetQuotes(os.Getenv("QUOTES"))
 	for _, v := range u {
 		name, _, percentStr, _, nowStr := quotes.FormatQuotesCoreData(v)
 		fmt.Println(name, percentStr+"%", nowStr)
