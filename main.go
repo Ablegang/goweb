cddfd47160411e0387adeeefb9a8592ab0e237c5
Ablegang <lvpg@weipaitang.com>
2021-01-06 12:00:03 +0800

    杭州房管局爬虫

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
-import "goweb/app"
+import (
+	"fmt"
+	"goweb/pkg/house"
+)
 
 func main() {
-	//driver, err := house.New(house.HzGovResource)
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//	return
-	//}
-	//
-	//list, err1 := driver.GetList("金雅苑", "1")
-	//if err1 != nil {
-	//	fmt.Println(err1.Error())
-	//}
-	//fmt.Println(list)
-	app.Start()
+	driver, err := house.New(house.HzGovResource)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	list, err1 := driver.GetList("金雅苑", "1")
+	if err1 != nil {
+		fmt.Println(err1.Error())
+	}
+
+	if list != nil {
+		for k,v := range list.List {
+			fmt.Println(k,v)
+		}
+	}
+	//app.Start()
 }
